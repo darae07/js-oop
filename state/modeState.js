830f72c31a5b9c560ac8713d3a7abb27941896e9
darae07 <leedr0730@gmail.com>
2021-09-02 23:27:11 +0900

    설명 주석 추가

--- state/modeState.js
+++ state/modeState.js
@@ -2,10 +2,12 @@ class ModeState {
   toggle (modeSwitch) {};
 };
 
+// 모드 - 상태와 묶어서 모드 갈아끼우는 메소드 제공
 class ModeStateLight extends ModeState {
   toggle (modeSwitch) {
     super.toggle();
     console.log('FROM LIGHT TO DARK');
+    // 각 상태별로 메소드 실행시 모드도 변경
     modeSwitch.setState(new ModeStateDark());
   };
 };
@@ -14,6 +16,7 @@ class ModeStateDark extends ModeState {
   toggle (modeSwitch) {
     super.toggle();
     console.log('FROM DARK TO LIGHT');
+    // 각 상태별로 메소드 실행시 모드도 변경
     modeSwitch.setState(new ModeStateLight());
   };
 };
--- state/modeSwitch.js
+++ state/modeSwitch.js
@@ -1,8 +1,10 @@
 const { ModeStateLight, ModeStateDark} = require('./modeState');
 
 class ModeSwitch {
+  // 상태에 모드를 지정하고
   #modeState = new ModeStateLight();
 
+  // 상태 변경시 모드도 변경됨.
   setState (_modeState) {
     this.#modeState = _modeState;
   };
